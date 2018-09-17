7db3eb4d5a47d555f0da4325c5f09098eb29ab38
Pedro Sousa <pedroteosousa@gmail.com>
2018-09-17 02:53:14 -0300

    Use new cross syntax

--- structures/Geometry.py
+++ structures/Geometry.py
@@ -156,7 +156,7 @@ class Segment():
 
     def __contains__(self, other):
         """ Check if a Point belongs to this segment. """
-        if (other - self.a) ^ self.pin() == 0 and abs(2 * other - self.a - self.b) <= self.len():
+        if (other - self.a) ** self.pin() == 0 and abs(2 * other - self.a - self.b) <= self.len():
             return True
         return False
 
@@ -166,7 +166,7 @@ class Segment():
         if isinstance(other, Point) and other in self:
             return other
         if isinstance(other, Segment):
-            if (other.a - self.a) ^ self.pin() == 0 and (other.b - self.a) ^ self.pin() == 0:
+            if (other.a - self.a) ** self.pin() == 0 and (other.b - self.a) ** self.pin() == 0:
                 p, q = sorted([other.a, other.b, self.a, self.b])[1: 3]
                 if p in self and p in other:
                     return Segment(p, q)
