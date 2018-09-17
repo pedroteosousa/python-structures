6a5ac528fd69025729791247c0da24e446eb4a90
Pedro Sousa <pedroteosousa@gmail.com>
2018-09-17 16:34:48 -0300

    Improve test for finding strongly connected components

--- tests/testGraph.py
+++ tests/testGraph.py
@@ -8,8 +8,8 @@ from structures.Graph import DirectedGraph
 class testDirectedGraph(unittest.TestCase):
     def test_strong_components(self):
         g = DirectedGraph()
-        edges = [(0, 1), (1, 3), (3, 0), (3, 2), (2, 4), (4, 2), (4, 5), (5, 6), (6, 7), (7, 6)]
+        edges = [(0, 1), (1, 3), (3, 0), (3, 2), (2, 4), (4, 2), (4, 5), (5, 6), (6, 7)]
         for e in edges:
             g.add_edge(*e)
         components = sorted([sorted(c) for c in g.strong_components()])
-        self.assertEqual(components, [[0, 1, 3], [2, 4], [5], [6, 7]])
+        self.assertEqual(components, [[0, 1, 3], [2, 4], [5], [6], [7]])
