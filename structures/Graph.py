f29747e82c3b6415fab53492dd0c017834ab0c76
Pedro Sousa <pedroteosousa@gmail.com>
2018-09-17 04:39:08 -0300

    Add DirectedGraph class and tests

---
+++ structures/Graph.py
@@ -0,0 +1,44 @@
+# pylint: disable=invalid-name
+""" Implementation of graph data structure. """
+
+class DirectedGraph():
+    """ Directed graph. """
+
+    def __init__(self):
+        self.__adj = {} # adjancency list
+
+    def addEdge(self, u, to, capacity=1, cost=1):
+        """ Add an edge to the graph """
+        edge = {'to': to, 'capacity': capacity, 'cost': cost}
+        if u not in self.__adj:
+            self.__adj[u] = []
+        self.__adj[u].append(edge)
+
+    def strongComponents(self):
+        """ Get strongly connected components. """
+        components = []
+
+        time = 0
+        seen, low = {}, {}
+        stack = []
+        def scc_dfs(node):
+            nonlocal time, seen, low, stack
+            seen[node] = low[node] = time = time + 1
+            stack.append(node)
+            for edge in self.__adj[node]:
+                if edge['to'] not in low:
+                    scc_dfs(edge['to'])
+                low[node] = min(low[node], low[edge['to']])
+            if low[node] == seen[node]:
+                new_component = []
+                while stack:
+                    new_component.append(stack.pop())
+                    low[new_component[-1]] = len(self.__adj) + 1
+                    if new_component[-1] == node:
+                        break
+                components.append(new_component)
+
+        for node in self.__adj:
+            if node not in seen:
+                scc_dfs(node)
+        return components
---
+++ tests/testGraph.py
@@ -0,0 +1,15 @@
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-public-methods
+
+import unittest
+from structures.Graph import DirectedGraph
+
+class testDirectedGraph(unittest.TestCase):
+    def test_strong_components(self):
+        g = DirectedGraph()
+        edges = [(0, 1), (1, 3), (3, 0), (3, 2), (2, 4), (4, 2), (4, 5), (5, 6), (6, 7), (7, 6)]
+        for e in edges:
+            g.addEdge(*e)
+        components = sorted([sorted(c) for c in g.strongComponents()])
+        self.assertEqual(components, [[0, 1, 3], [2, 4], [5], [6, 7]])
